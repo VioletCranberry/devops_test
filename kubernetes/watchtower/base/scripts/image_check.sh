2074fae5e166a8b506e8c07b679b963a6ac5c38d
Fedor Zhdanov <zh6an0w.fedor@gmail.com>
2020-12-28 15:18:35 +0100

    auto deploy

---
+++ kubernetes/watchtower/base/scripts/image_check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function get_sha_local() {
+  local repo="$1" result
+  result="$(kubectl get pods --namespace default -o json | `
+  `jq -r ".items[] .status.containerStatuses[] | select(.imageID | contains(\"${repo}\")) | .imageID" | sort -u)"
+  echo "$result" | awk -F '@' '{ print $2 }'
+}
+
+function get_sha_remote() {
+  local repo="$1" region="$2" result
+  result="$(aws ecr describe-images --repository-name "$repo" --region "$region" | `
+  `jq -r '.imageDetails[0] .imageDigest')"
+  echo "$result"
+}
+
+function main() {
+
+  local repo='internal-ervcp_repo' region='eu-central-1'
+  local deployment='ervcp'
+
+  if [ "$(get_sha_local "$repo")" != "$(get_sha_remote "$repo" "$region")" ]; then
+    echo "new image available"
+    kubectl rollout restart deployment "$deployment"
+  else
+    echo "no new image available"
+  fi;
+}
+
+main
